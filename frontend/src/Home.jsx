a3d474aef4273bd30373383991d43584354f3868
Kathea San Andres <katheasanandres@gmail.com>
2026-05-12 09:56:48 +0800

    CASE SENSITIVITY!

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -6,7 +6,7 @@ import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet.heat';
 import 'leaflet/dist/leaflet.css';
-import { useLocation } from './hooks/UseLocation.js';
+import { useLocation } from './hooks/UseLocation';
 import { useSettings } from './hooks/UseSettings';
 import ReportModal from './ReportModal';
 import './Home.css';
